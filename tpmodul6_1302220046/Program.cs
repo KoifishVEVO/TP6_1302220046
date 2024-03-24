bf55c0496dd7501cfd4694771d61450ad588ffe8
KoifishVEVO <farhan210604@gmail.com>
2024-03-24 23:34:43 +0700

    Menambahkan class SayaTubeVideo

--- tpmodul6_1302220046/Program.cs
+++ tpmodul6_1302220046/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq.Expressions;
+
+
+
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Muhammad Farhan Ismali Fentarto");
+        video.IncreasePlayCount(10);
+        video.PrintVideoDetails();
+
+    }
+}
---
+++ tpmodul6_1302220046/SayaTubeVideo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+
+    public SayaTubeVideo(string judul)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 99999);
+        this.title = judul;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int playCount)
+    {
+                this.playCount += playCount;
+            
+
+
+
+
+    }
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID: " + this.id);
+        Console.WriteLine("Judul: " + this.title);
+        Console.WriteLine("Play Count: " + this.playCount);
+
+    }
+}
